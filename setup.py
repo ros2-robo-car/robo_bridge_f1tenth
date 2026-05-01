81ac1c23aadd903fe2bf652f47cfe12819d14e66
tgds03 <tgds03@naver.com>
2026-05-01 19:38:55 +0900

    Init

---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import find_packages, setup
+
+package_name = 'f110_gym_client'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='kim',
+    maintainer_email='kim@todo.todo',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+            'f110_gym_client = f110_gym_client.f110_gym_client:main'
+        ],
+    },
+)
